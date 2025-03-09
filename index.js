cd5db9fe059f87ae8e75c9acc6892c4b155e5d1b
Zouhir Bensmaia <freelancebenz@gmail.com>
2025-03-09 17:20:31 -0400

    pruned all articles not indexed in aiff to mp3 subject

--- index.js
+++ index.js
@@ -106,6 +106,13 @@ const goneUrls = [
   "/seo/citation-listing-identifier/bidblock/1",
   "/tiroir1/avertissement-legal",
   "/seo/citation-listing-identifier/earnanswers/1",
+  "/blog/audio-conversion/blog-posting/convert-aiff-to-mp3-on-mac",
+  "/blog/audio-conversion/blog-posting/how-to-convert-aiff-to-mp3-free",
+  "/blog/audio-conversion/blog-posting/how-to-convert-aiff-to-mp3-on-macbook",
+  "/blog/audio-conversion/blog-posting/can-vlc-convert-aiff-to-mp3",
+  "/blog/audio-conversion/blog-posting/is-aiff-mp3",
+  "/blog/audio-conversion/blog-posting/is-aiff-the-same-as-mp3",
+  "/blog/audio-conversion/blog-posting/can-audacity-convert-aiff-to-mp3",
 ];
 
 
