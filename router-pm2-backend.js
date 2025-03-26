2ceb0093424160d06d8410c1585fa0af982bf758
Zouhir Bensmaia <freelancebenz@gmail.com>
2025-03-26 16:00:01 -0400

    ok

--- router-pm2-backend.js
+++ router-pm2-backend.js
@@ -4,6 +4,7 @@ const router_pm2 = express.Router();
 require('dotenv').config();
 
 
+
 router_pm2.post('/monitoring-notification', (req, res) => {
   console.log("\n/pm2/monitoring-notification endpoint HIT!\n")
   const { process } = req.body;
@@ -15,20 +16,6 @@ router_pm2.post('/monitoring-notification', (req, res) => {
   
   res.send('OK');
 });
-
-// router_pm2.post('/monitoring-notification', (req, res) => {
-//   const { event, process } = req.body;
-  
-//   // Trigger for both crashes AND stops
-//   if (event === 'exit' || event === 'stop') {
-//     require('child_process').exec(
-//       `echo "PM2 ALERT: ${process.name} (${event})" | mail -s "ALERT" 8196644106@txt.bell.ca`,
-//       (err) => { if (err) console.error(err) }
-//     );
-//   }
-  
-//   res.send('OK');
-// });
 
 
 module.exports = router_pm2;
