55950881a1860e7c7aae1536a35b147e7bd974e4
Zouhir Bensmaia <freelancebenz@gmail.com>
2025-07-23 14:53:13 -0400

    ok

--- affiliatelinks-backend.js
+++ affiliatelinks-backend.js
@@ -331,14 +331,14 @@ affiliateLinks_app_router.get('/thirty-two', (req,res)=>{
 
 // https://opom.ca/ral/thirty-three
 // http://localhost:3005/ral/thirty-three
-affiliateLinks_app_router.get('/thirty-three', middleware4.mid1, (req,res)=>{
-  return res.render('temporal-micro-workers-drywallkingston', { ...res.locals.index_page_data });
-})
+// affiliateLinks_app_router.get('/thirty-three', middleware4.mid1, (req,res)=>{
+//   return res.render('temporal-micro-workers-drywallkingston', { ...res.locals.index_page_data });
+// })
 
 // https://opom.ca/ral/thirty-three
-// affiliateLinks_app_router.get('/thirty-three', (req,res)=>{
-//   return res.redirect('https://maps.app.goo.gl/naUAWDJeyxbqCKMx5');
-// })
+affiliateLinks_app_router.get('/thirty-three', (req,res)=>{
+  return res.redirect('https://maps.app.goo.gl/naUAWDJeyxbqCKMx5');
+})
 
 
 
